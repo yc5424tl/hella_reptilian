89c5c3ebe6194c435f8931317c4f58de64360dcb
yc5424tl <yc5424tl@go.minneapolis.edu>
2018-03-05 22:48:57 -0600

    i may have had bad syntax

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import UniqueConstraint
+from sqlalchemy.orm import relationship
 
 from manage import db, app
 
@@ -11,8 +12,8 @@ class Show(db.Model):
     show_date = db.Column(db.Date, nullable=False)
     show_venue = db.Column(db.String, nullable=False)
 
-    sales = db.relationship('sales', backref='shows', passive_deletes=True)
-
+    # sales = db.relationship('sales', backref='shows', passive_deletes=True)
+    sales = relationship("Sales", cascade="save-update, merge, delete")
 
     def __init__(self, date, venue):
         self.show_date = date
@@ -32,7 +33,8 @@ class Merch(db.Model):
     merch_cost = db.Column(db.Float, nullable=False)
     merch_descr = db.Column(db.VARCHAR, nullable=False)
 
-    sales = db.relationship('sales', backref='merch', passive_deletes=True)
+    sales = relationship('Sales', cascade="save-update, merge, delete")
+    # sales = db.relationship('sales', backref='merch', passive_deletes=True)
 
     def __init__(self, name, cost, descr):
         self.merch_name = name
@@ -50,9 +52,12 @@ class Sales(db.Model):
     __table_args__ = (UniqueConstraint('show_id', 'merch_id', 'merch_name', name='sales_table_unique_constraint_show-id_merch-id_merch-name'),)
 
     sales_id = db.Column(db.Integer, primary_key=True)
-    show_id = db.Column(db.Integer, db.ForeignKey('shows.show_id', onupdate="CASCADE", ondelete="CASCADE", nullable=False))
-    merch_id = db.Column(db.Integer, db.ForeignKey('merch.merch_id', onupdate="CASCADE", nullable=False))
-    merch_name = db.Column(db.String, db.ForeignKey('merch.merch_name', onupdate="CASCADE", nullable=False))
+    # show_id = db.Column(db.Integer, db.ForeignKey('shows.show_id', onupdate="CASCADE", ondelete="CASCADE", nullable=False))
+    # merch_id = db.Column(db.Integer, db.ForeignKey('merch.merch_id', onupdate="CASCADE", nullable=False))
+    # merch_name = db.Column(db.String, db.ForeignKey('merch.merch_name', onupdate="CASCADE", nullable=False))
+    show_id = db.Column(db.Integer, db.ForeignKey('shows.show_id'), nullable=False)
+    merch_id = db.Column(db.Integer, db.ForeignKey('merch.merch_id'), nullable=False)
+    merch_name = db.Column(db.String, db.ForeignKey('merch.merch_name'), nullable=False)
     num_sold = db.Column(db.Integer, default=0, nullable=False)
 
 
