9949664c861ac20e33e7652bd19ffe3221eeb1b1
yc5424tl <yc5424tl@go.minneapolis.edu>
2018-03-05 18:32:55 -0600

    in py terminal, ran: from hella_reptilian import db; db.create_all()

--- hella_reptilian.py
+++ hella_reptilian.py
@@ -8,10 +8,11 @@ app = Flask(__name__)
 Bootstrap(app)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://frwarqgpoagzfk:1739020739f0fabf22d558773024fbf2ea9738941d8082fe216bc5cf030971fd@ec2-54-204-46-60.compute-1.amazonaws.com:5432/d7igmaohp1q70m'
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 #sECRET_KEY = os.environ['flask-secret-key']
 app.config.from_object(__name__)
 db = SQLAlchemy(app)
+
 
 from models import Merch, Sales, Show
 
