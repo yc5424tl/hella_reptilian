46fa176518cca77d0c97bb946853a5b1b4e82259
yc5424tl <yc5424tl@go.minneapolis.edu>
2018-03-06 14:31:35 -0600

    new approach

--- models.py
+++ models.py
@@ -9,11 +9,10 @@ class Show(db.Model):
 
     __tablename__ = 'shows'
 
-    show_id = db.Column(db.Integer, primary_key=True)
+    show_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     show_date = db.Column(db.Date, nullable=False)
     show_venue = db.Column(db.String, nullable=False)
-
-    rel_sales = db.relationship("Sales", back_populates="rel_show_id")
+    sales = db.relationship('Sales', backref='shows', cascade='all, delete orphan', lazy='dynamic')
 
     # sales = db.relationship('sales', backref='shows', passive_deletes=True)
     # sales = db.relationship("Sales", cascade="save-update, merge, delete")
@@ -30,13 +29,11 @@ class Merch(db.Model):
 
     __tablename__ = 'merch'
 
-    merch_id = db.Column(db.Integer, primary_key=True)
+    merch_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     merch_name = db.Column(db.String, nullable=False, unique=True)
     merch_cost = db.Column(db.Float, nullable=False)
     merch_descr = db.Column(db.VARCHAR, nullable=False)
-
-    rel_sales_id = db.relationship("Sales.merch_id", back_populates="rel_merch_id")
-    rel_sales_name = db.relationship("Sales.merch_name", back_populates="rel_merch_name")
+    sales = db.relationship('Sales', backref='merch', cascade='all, delete-orphan', lazy='dynamic')
     #
     # sales = db.relationship('Sales', cascade="save-update, merge, delete")
     # sales = db.relationship('sales', backref='merch', passive_deletes=True)
@@ -53,34 +50,46 @@ class Merch(db.Model):
 
 class Sales(db.Model):
 
-    __tablename__ = 'sales'
+    # __tablename__ = 'sales'
     # __table_args__ = (UniqueConstraint('show_id', 'merch_id', 'merch_name', name='sales_table_unique_constraint_show-id_merch-id_merch-name'),)
 
-    sales_id = db.Column(db.Integer, primary_key=True)
+    #sales_id = db.Column(db.Integer, primary_key=True)
     # show_id = db.Column(db.Integer, db.ForeignKey('shows.show_id', onupdate="CASCADE", ondelete="CASCADE", nullable=False))
     # merch_id = db.Column(db.Integer, db.ForeignKey('merch.merch_id', onupdate="CASCADE", nullable=False))
     # merch_name = db.Column(db.String, db.ForeignKey('merch.merch_name', onupdate="CASCADE", nullable=False))
+    #show_id = db.Column(db.Integer, db.ForeignKey('shows.show_id'), nullable=False)
+    # merch_id = db.Column(db.Integer, db.ForeignKey('merch.merch_id'), nullable=False)
+
+
+    __tablename__ = 'sales'
+    __table_args__ = (UniqueConstraint('show_id', 'merch_name', name='unique_show_merch'),)
+    sale_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     show_id = db.Column(db.Integer, db.ForeignKey('shows.show_id'), nullable=False)
-    merch_id = db.Column(db.Integer, db.ForeignKey('merch.merch_id'), nullable=False)
     merch_name = db.Column(db.String, db.ForeignKey('merch.merch_name'), nullable=False)
     num_sold = db.Column(db.Integer, default=0, nullable=False)
 
-    rel_show_id = db.relationship("Sales", foreign_keys=show_id, back_populates="rel_sales")
-    rel_merch_id = db.relationship("Merch", foreign_keys=merch_id, back_populates="rel_sales_id")
-    rel_merch_name = db.relationship("Merch", foreign_keys=merch_name, back_populates="rel_sales_name")
+    def __init__(self, show_id, merch_name, num_sold):
+        self.show_id = show_id
+        self.merch_name = merch_name
+        self.num_sold = num_sold
+
+    def __repr__(self):
+        template = 'show_id: {} merch_name: {} number_sold: {}'
+        return template.format(self.show_id, self.merch_name, self.num_sold)
+    # rel_show_id = db.relationship("Sales", foreign_keys=show_id, back_populates="rel_sales")
+    # rel_merch_id = db.relationship("Merch", foreign_keys=merch_id, back_populates="rel_sales_id")
+    # rel_merch_name = db.relationship("Merch", foreign_keys=merch_name, back_populates="rel_sales_name")
 
     # show = db.relationship(Show, foreign_keys=show_id)
     # item_id = db.relationship(Merch, foreign_keys=merch_id)
     # item_name = db.relationship(Merch, foreign_keys=merch_name)
 
-
-
-    def __init__(self, show_id, merch_id, merch_name, num_sold):
-        self.show_id = show_id
-        self.merch_id = merch_id
-        self.merch_name = merch_name
-        self.num_sold = num_sold
-
-    def __repr__(self):
-        template = 'show_id: {} merch_id: {} merch_name: {} number_sold: {}'
-        return template.format(self.show_id, self.merch_id, self.merch_name, self.num_sold)
+    # def __init__(self, show_id, merch_id, merch_name, num_sold):
+    #     self.show_id = show_id
+    #     self.merch_id = merch_id
+    #     self.merch_name = merch_name
+    #     self.num_sold = num_sold
+    #
+    # def __repr__(self):
+    #     template = 'show_id: {} merch_id: {} merch_name: {} number_sold: {}'
+    #     return template.format(self.show_id, self.merch_id, self.merch_name, self.num_sold)
